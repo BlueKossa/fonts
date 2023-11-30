6a892f94ca79450a762afff5d1310bda4afacb72
BlueKossa <dexter@stormdal.se>
2023-12-01 00:21:22 +0100

    sha256

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
           src = pkgs.fetchzip {
             url =
               "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Monaspace.zip";
-            sha256 = "sha256-YcZUKzRskiqmEqVcbK/XL6ypsNMbY49qJYFG3yZVF78=";
+            sha256 = "sha256-bfe5670d8dd98c51d911df7ea322f05f67141a20aaa667925bb1703de1a9b6c2";
             stripRoot = false;
           };
           installPhase = ''
